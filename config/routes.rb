a4c3d8b49eb75bee6797aab1c655b82da8fddce5
Josiah Williams <jjosiahww@gmail.comgit config --global user.email jjosiahww@gmail.com>
2015-02-11 10:38:52 -0600

    Added Bootstrap and customization, Added Sign Up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     root "pages#home"
     get "about" => "pages#about" # Creates about_path
+    get "sign_up" => "pages#sign_up" # Creates sign_up_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
